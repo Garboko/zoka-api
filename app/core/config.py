60959b3a284eacd7518ac276a1e979972bc78a90
ZONGO <aimezongo2@gmail.com>
2025-10-14 18:15:56 +0200

    feat: add config

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from pydantic-settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+    PROJECT_DESCRIPTION: str
+    VERSION: str
+    API_V1_STR: str
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    BACKEND_CORS_ORIGINS: list
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+        extra = "ignore"
+
+settings = Settings()
+
+
+
