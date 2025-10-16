37da8d0b3f1934777161015ee17f210fea10401b
ZONGO <aimezongo2@gmail.com>
2025-10-16 13:09:07 +0200

    refactor: update models __init__ with new structure

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,41 +1,29 @@
-from .user import User
-from .organization import Organization
-from .project import Project
-from .form import Form
-from .form_version import FormVersion
-from .submission import Submission
-from .media_file import MediaFile
-from .user_assignment import UserAssignment
-from .form_assignment import FormAssignment
-from .form_field import FormField
-from .audit_log import AuditLog
-from .sync_status import SyncStatus
-from .analytics_dashboard import AnalyticsDashboard, DashboardWidget
-from .data_summary import DataSummary
-from .report_template import ReportTemplate, ReportJob
-from .api_token import APIToken, APILog
-from .webhook import Webhook, WebhookLog
+from .users import User
+from .enumerators import Enumerator
+from .projects import Project
+from .forms import Form
+from .form_access import FormAccess
+from .submissions import Submission
+from .media_files import MediaFile
+from .email_verifications import EmailVerification
+from .form_statistics import FormStatistic
+from .devices import Device
+from .form_versions import FormVersion
+from .submission_reviews import SubmissionReview
+from .audit_logs import AuditLog
 
 __all__ = [
     "User",
-    "Organization", 
+    "Enumerator",
     "Project",
     "Form",
-    "FormVersion",
-    "Submission", 
+    "FormAccess",
+    "Submission",
     "MediaFile",
-    "UserAssignment",
-    "FormAssignment",
-    "FormField",
+    "EmailVerification",
+    "FormStatistic",
+    "Device",
+    "FormVersion",
+    "SubmissionReview",
     "AuditLog",
-    "SyncStatus",
-    "AnalyticsDashboard",
-    "DashboardWidget",
-    "DataSummary",
-    "ReportTemplate", 
-    "ReportJob",
-    "APIToken",
-    "APILog",
-    "Webhook",
-    "WebhookLog"
 ]
