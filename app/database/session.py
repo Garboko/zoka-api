1cec5cf3cfaa0d4393e0298268274ea8fed0df03
ZONGO <aimezongo2@gmail.com>
2025-10-17 10:48:57 +0200

    refactor: switch from asyncpg to psycopg2 for synchronous database

--- app/database/session.py
+++ app/database/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(url=settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(url=settings.DATABASE_URL, pool_pre_ping=True, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
