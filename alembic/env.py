4813fa34d4cdd6b10d70190f64d3614c704ed5ce
ZONGO <aimezongo2@gmail.com>
2025-10-17 10:48:57 +0200

    refactor: update Alembic migration for synchronous database

--- alembic/env.py
+++ alembic/env.py
@@ -2,15 +2,14 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-import os 
 import sys
+import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app.core.config import settings
-from app.database.base import Base  
-
 from alembic import context
+from app.database.base import Base
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +20,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.ALEMBIC_URL)
+config.set_main_option('sqlalchemy.url', settings.DATABASE_URL)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
